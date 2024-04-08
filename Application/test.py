e652335f90c687d36d441a0c743652cb6951feb4
kwakchanhyuk <kwakchanhyuk@naver.com>
2024-04-08 22:49:08 +0900

    [FEAT]Applicaion method update

--- Application/test.py
+++ Application/test.py
@@ -35,74 +35,64 @@
 #     print("Program Closed")
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QAction, qApp
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QAction, qApp, QDesktopWidget
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QCoreApplication
 
 
-class MyApp1(QWidget):
+class MyApp(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.Quit_button()
+        self.menu_bar()
         self.initUI()
 
+    #window 기본 setting
     def initUI(self):
-        
-        #window 기본 setting
-        self.setWindowTitle('Test App')
+        self.setWindowTitle('My Application')
         self.setWindowIcon(QIcon("C:/workspace/ML&DL/Application/test.png"))
         self.setGeometry(300, 300, 1000, 500)
-        
-        #Quit button
-        btn = QPushButton("Quit", self)
-        btn.move(300, 300)
+
+        self.statusBar().showMessage('This is Statusbar')
+        self.center()
+        self.show()
+
+    #center window
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+
+    #button method(quit)
+    def Quit_button(self):
+        btn = QPushButton('Quit', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.move(50,50)
         btn.resize(btn.sizeHint())
         btn.clicked.connect(QCoreApplication.instance().quit)
-        
-        
-        
-        self.show()
 
-class MyApp2(QMainWindow):
-
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.statusBar().showMessage('This is Statusbar')
-
-        self.setWindowTitle('Statusbar')
-        self.setGeometry(300, 300, 300, 200)
-        self.show()
-
-class MyApp3(QMainWindow):
-
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
+    #menu_bar
+    def menu_bar(self):
         exitAction = QAction(QIcon("C:/workspace/ML&DL/Application/test2.png"), 'Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(qApp.quit)
 
-        self.statusBar()
+        menuBar = self.menuBar()
+        menuBar.setNativeMenuBar(False)
 
-        menubar = self.menuBar()
-        menubar.setNativeMenuBar(False)
-        filemenu = menubar.addMenu('&File')
+        filemenu = menuBar.addMenu('&File')
         filemenu.addAction(exitAction)
 
-        self.setWindowTitle('Menubar')
-        self.setGeometry(300, 300, 300, 200)
-        self.show()
-
+        edit_menu = menuBar.addMenu('&Edit')
+        edit_menu.addAction(exitAction)
 
 if __name__ == '__main__':
    app = QApplication(sys.argv)
-   ex = MyApp3()
+   ex = MyApp()
    sys.exit(app.exec_())
 
 
